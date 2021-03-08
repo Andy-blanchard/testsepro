40860ff62a126be7d42a62bcc01acc0a97a26bac
Andy Blanchard <59825040+Andy-blanchard@users.noreply.github.com>
2021-03-08 11:01:50 +0100

    Test module II-Lifting

---
+++ s_delivery_gs1_labels/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import stock_quant
+from . import product_product
+from . import product_template
+from . import delivery_gs1_label
+from . import product_packaging
+from . import barcodes
+from . import stock_picking
+from . import stock_move
+from . import stock_move_line
